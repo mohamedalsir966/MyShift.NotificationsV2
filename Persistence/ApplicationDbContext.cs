c18940c486d7d6f35ba864a0071b321900762d4f
Mohamed Alsir <mohamed.alsir@cxunicorn.com>
2022-08-28 13:29:46 +0400

    commit

---
+++ Persistence/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Domain;
+using Domain.BotDB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=NotificationDBV2;Integrated Security=True");
+        }
+
+        public DbSet<ShiftEntityLog> ShiftEntityLog { get; set; }
+        public DbSet<Root> Roots { get; set; }
+        public DbSet<Bot> Bots { get; set; }
+        public DbSet<Conversation> Conversations { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ShiftEntityLog>()
+              .HasIndex(x => x.Id);
+
+        }
+
+    }
+}
---
+++ Persistence/Repositories/ILogsRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public interface ILogsRepository
+    {
+       Task<List<ShiftEntityLog>> GetLogsQueries();
+       Task <string> UpdateLogsCommand(List<ShiftEntityLog> items);
+        Task<ConversationReference> GetConversationReference();
+        Task<ConversationReference> AddConversationReferenceAsync(ConversationReference conversationReference);
+    }
+}
