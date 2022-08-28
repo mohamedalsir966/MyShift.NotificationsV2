eb52b63c5d25974441adf63f7f192b238d18c813
Mohamed Alsir <mohamed.alsir@cxunicorn.com>
2022-08-28 13:54:25 +0400

    ttt

---
+++ MyShift.Notifications/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using MyShift.Notifications;
+using MyShift.Notifications.Service;
+using Persistence.Repositories;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder;
+using MyShift.Notifications.Bot;
+using MyShift.Notifications.Helpers.Notification;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace MyShift.Notifications
+{
+  public  class Startup : FunctionsStartup
+    {
+
+       
+       
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddEnvironmentVariables()
+               .Build();
+            //builder.Services.Configure<RepositoryOptions>(config.GetSection("repositoryOptions"));
+            //builder.Services.AddBlobClient();INotificationDataHelper
+
+            //builder.Services.AddTableClient();
+
+            builder.Services.AddScoped<IService, ServiceEng>();
+            builder.Services.AddScoped<ILogsRepository, LogsRepository>();
+            //builder.Services.AddScoped<INotificationService, NotificationService>();
+           // builder.Services.AddScoped<IUsersDataRepository, UsersDataRepository>();
+            builder.Services.AddScoped<INotificationDataHelper, NotificationDataHelper>();
+           //builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+           // builder.Services.AddScoped<ILoggingRepository, LoggingRepository>();
+            builder.Services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+            builder.Services.AddTransient<IBot, ProactiveBot>();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Data Source=.;Initial Catalog=NotificationDBV2;Integrated Security=True"));
+
+        }
+    }
+}
